e03f29ba280c0c7e436f132eb2cb1615e0dc6cdd
eug <eug@rubberflat.com>
2020-04-04 21:47:47 -0700

    Version 1. Desktop basics are in.

---
+++ src/comix.js
@@ -0,0 +1,91 @@
+function createTitlePage(issues) {
+  var container = document.createElement('div');
+  container.className = 'title-page';
+
+  for (var issueIndex = 0; issueIndex < issues.length; issueIndex++) {
+    var issue = issues[issueIndex];
+    var img = document.createElement('img');
+    img.src = issue.cover;
+    img.title = issue.title;
+
+    var link = document.createElement('a');
+    link.className = 'issue';
+    link.appendChild(img);
+    link.href = '#' + issueIndex;
+
+    container.appendChild(link);
+  }
+
+  return container;
+}
+
+function createCell() {
+  var back = document.createElement('a');
+  back.id = 'back';
+  back.className = 'back-arrow';
+
+  var forward = document.createElement('a');
+  forward.id = 'forward';
+  forward.className = 'forward-arrow';
+
+  var cell = document.createElement('img');
+  cell.id = 'cell';
+  cell.className = 'cell';
+
+  var progress = document.createElement('div');
+  progress.id = 'progress';
+  progress.className = 'progress';
+
+  var stretchy = document.createElement('div');
+  stretchy.className = 'stretchy';
+
+  var home = document.createElement('a');
+  home.className = 'home';
+  home.href = '#';
+
+  var container = document.createElement('div');
+  container.className = 'page';
+  container.appendChild(back);
+  container.appendChild(progress);
+  container.appendChild(stretchy);
+  container.appendChild(cell);
+  container.appendChild(forward);
+  container.appendChild(home);
+
+  return container;
+}
+
+function drawCell(container, issues, issueIndex, pageIndex) {
+  issueIndex = Number(issueIndex) || 0;
+  pageIndex = Number(pageIndex) || 0;
+  if (!issues[issueIndex]) {
+    issueIndex = 0;
+  }
+  if (!issues[issueIndex].pages[pageIndex]) {
+    pageIndex = 0;
+  }
+
+  var back = document.getElementById('back');
+  var forward = document.getElementById('forward');
+  var cell = document.getElementById('cell');
+  var progress = document.getElementById('progress');
+
+  cell.src = issues[issueIndex].pages[pageIndex];
+
+  var pages = issues[issueIndex].pages.length;
+  progress.style.width = (100 * (pageIndex + 1) / pages) + '%';
+
+  if (!issues[issueIndex].pages[pageIndex - 1]) {
+    var previousIssue = (issues.length + issueIndex - 1) % issues.length;
+    back.href = '#' + previousIssue + ':' + (issues[previousIssue].pages.length - 1);
+  } else {
+    back.href = '#' + issueIndex + ':' + (pageIndex - 1);
+  }
+
+  if (!issues[issueIndex].pages[pageIndex + 1]) {
+    var nextIssue = (issueIndex + 1) % issues.length;
+    forward.href = '#' + nextIssue;
+  } else {
+    forward.href = '#' + issueIndex + ':' + (pageIndex + 1);
+  }
+}
---
+++ update.py
@@ -0,0 +1,33 @@
+from json import dumps
+from os import listdir
+from os.path import isfile, join
+from re import sub
+
+OUTPUT = join('src','data.js')
+INPUT_PATH = 'comix'
+IMAGE_EXT = ('.png', '.jpg', '.jpeg', '.gif', '.svg')
+INT_SORT = lambda x: int(sub(r'\D*', '', x))
+
+data = []
+issues = [f for f in listdir(INPUT_PATH) if not isfile(join(INPUT_PATH, f))]
+# Sort using numbers, otherwise page '10' will show up before page '2'
+issues.sort(key=INT_SORT)
+
+for title in issues:
+  path = join(INPUT_PATH, title)
+  pages = []
+  cover = None
+  for f in listdir(path):
+    if not isfile(join(path, f)) or not f.lower().endswith(IMAGE_EXT):
+      continue
+    image_path = '{0}/{1}/{2}'.format(INPUT_PATH, title, f)
+    if 'cover' in f:
+      cover = image_path
+    else:
+      pages.append(image_path)
+
+  pages.sort(key=INT_SORT)
+  data.append({'title': title, 'cover':cover, 'pages': pages})
+
+with open(OUTPUT, 'w') as f:
+  f.write('var COMIX_DATA = {0};'.format(dumps(data)))
