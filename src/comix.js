8e19a00cd22f165a2f1b10e5d3a4a4eab4c74087
eug <eug@rubberflat.com>
2021-11-27 12:58:55 -0800

    Update new buy code and link

--- src/comix.js
+++ src/comix.js
@@ -2,6 +2,8 @@ function createTitlePage(issues) {
   var container = document.createElement('div');
   container.className = 'title-page';
 
+  // container.appendChild(getBookLink());
+
   for (var issueIndex = issues.length - 1; issueIndex >= 0; issueIndex--) {
     var issue = issues[issueIndex];
     var img = document.createElement('img');
@@ -17,6 +19,14 @@ function createTitlePage(issues) {
   }
 
   return container;
+}
+
+function getBookLink() {
+  var link = document.createElement('a');
+  link.href = 'book.html';
+  link.className = 'book-link';
+  link.innerHTML = 'Dig it? Get <span class="book-name"><img src="src/book_icon.png"/> Covid Comix (the book)</span> here!';
+  return link;
 }
 
 function createCell() {
